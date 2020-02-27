031be27f79d8c84646ff9f123f33fc79e03526b6
evolpi <everton.volpi@gmail.com>
2020-02-27 16:51:00 -0500

    hidden cart panel created; product quantity incrementer and decrementer improved; general design of products area improved

--- js/script.js
+++ js/script.js
@@ -6,22 +6,51 @@ document.querySelectorAll('.decrease').forEach(item => {
     item.addEventListener('click', decrease);
 });
 
-function increase(ev) {
-    var product = ev.target.getAttribute('product');
-    console.log(product);
-    var quantity = document.querySelector(`#${product}`).value;
-    quantity++
-    document.querySelector(`#${product}`).value = quantity;
+document.querySelectorAll('.changeQty').forEach(item => {
+    item.addEventListener('change', quantityInput);
+});
+
+document.querySelectorAll('.hideShowCart').forEach(item => {
+    item.addEventListener('click', showHideCart);
+});
+
+function increase() {
+    var quantity = document.querySelector(`#${this.getAttribute('product')}`).value;
+    var item = document.querySelector(`#${this.getAttribute('refreshValue')}`);
+    quantity++;
+    changeItemTotal(quantity, item);
+    document.querySelector(`#${this.getAttribute('product')}`).value = quantity;
 }
 
-function decrease(ev) {
-    var product = ev.target.getAttribute('product');
-    var quantity = document.querySelector(`#${product}`).value;
-    if(quantity == 1) {
+function decrease() {
+    var quantity = document.querySelector(`#${this.getAttribute('product')}`).value;
+    var item = document.querySelector(`#${this.getAttribute('refreshValue')}`);
+    if(quantity <= 1) {
         quantity = 1;
     }
     else {
         quantity--
+        changeItemTotal(quantity, item)
     }
-    document.querySelector(`#${product}`).value = quantity;
+    document.querySelector(`#${this.getAttribute('product')}`).value = quantity;
+}
+
+function quantityInput() {
+    if (this.value < 1) {
+        this.value = 1;
+    }
+    var quantity = this.value;
+    var item = document.querySelector(`#${this.getAttribute('refreshValue')}`);
+    changeItemTotal(quantity, item);    
+}
+
+function showHideCart() {
+    document.querySelector('#cart').classList.toggle('hide');
+    document.querySelector('.showCart').classList.toggle('hide');
+}
+
+function changeItemTotal(quantity, item) {
+    var price = parseFloat(item.getAttribute('price'));
+    var total = quantity * price;
+    item.innerHTML = total.toFixed(2);
 }
